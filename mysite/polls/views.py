51e6d4ad041f9c0ff92cbad75e05f5f3ea520449
Iñaki Zelayeta <zelayetai9@gmail.com>
2024-04-13 16:42:24 -0300

    [Parte 1.10] escribiendo vistas que realmente hagan algo

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-
+from .models import Question
+from django.template import loader
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
@@ -18,3 +19,16 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
